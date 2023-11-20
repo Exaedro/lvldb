dd5d697c13d11f547a3eff61e268ebac09203a92
Daniel Aguilera <da.ale.19777@gmail.com>
2023-11-19 19:33:38 -0500

    fix imports

--- src/main/java/com/exaedro/lvltierlits/service/PlayerService.java
+++ src/main/java/com/exaedro/lvltierlits/service/PlayerService.java
@@ -3,11 +3,9 @@ package com.exaedro.lvltierlits.service;
 import com.exaedro.lvltierlits.entity.PlayerEntity;
 import com.exaedro.lvltierlits.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
-import java.util.List;
+
 import java.util.Optional;
 
 @Service
--- src/main/java/com/exaedro/lvltierlits/web/controllers/PlayerController.java
+++ src/main/java/com/exaedro/lvltierlits/web/controllers/PlayerController.java
@@ -3,11 +3,9 @@ package com.exaedro.lvltierlits.web.controllers;
 import com.exaedro.lvltierlits.entity.PlayerEntity;
 import com.exaedro.lvltierlits.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
